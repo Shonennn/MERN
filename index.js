3a302a6cb3a581467bfac92308a5e1146c36f7dd
Shonennn <Shonennn@users.noreply.github.com>
2022-08-18 16:24:37 -0400

    s

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const passport = require("passport");
+const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const keys = require('./config/keys');
 
