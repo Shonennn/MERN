4727832624fd3a8e155ab5c44dd1789cdf7f1c5b
Shonennn <Shonennn@users.noreply.github.com>
2022-09-09 18:23:42 -0400

    payment functions

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cookieSession = require('cookie-session');
 const passport = require('passport');
+const bodyParser = require('body-parser');
 const keys = require('./config/keys');
 require('./models/User');
 require('./services/passport');
@@ -10,6 +11,8 @@ mongoose.connect(keys.mongoURI);
 
 const app = express();
 
+
+app.use(bodyParser.json());
 app.use(
     cookieSession({
         maxAge: 30 * 24 * 60 * 60 * 1000,
@@ -20,7 +23,8 @@ app.use(
 app.use(passport.initialize());
 app.use(passport.session());
 
-require("./routes/authRouths")(app);
+require("./routes/authRoutes")(app);
+require('./routes/billingRoutes')(app);
 
 
 
