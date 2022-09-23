cdfdd8cf8b8d47a2a08b8e15d09838523997c4b1
Shonennn <Shonennn@users.noreply.github.com>
2022-09-22 23:48:19 -0400

    Servey and validate

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -6,7 +6,7 @@ import Header from './Header';
 import Landing from './Landing';
 
 import Dashboard from './Dashboard';
-const SurveyNew = () => <h2>SurveyNew</h2>;
+import SurveyNew from './surveys/SurveyNew';
 class App extends Component {
   componentDidMount() {
     this.props.fetchUser(); 
@@ -15,7 +15,7 @@ class App extends Component {
     return (
       <div className="container">
         <BrowserRouter>
-          <div>
+          <div className='container'>
             <Header />
             <Route exact path="/" component={Landing} />
             <Route exact path="/surveys" component={Dashboard} />
